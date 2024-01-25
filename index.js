57b44842cd77d46ff6a6aacda86704ed56c13b68
José Carlos Ferrari <licoverpa321@gmail.com>
2024-01-24 22:33:10 -0300

    adding front-end test route for owners

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post("/api/login", auth.generateToken);
 
 //front-end integration routes
 app.get("/api/cars/teste", carController.getCars);
+app.get("/api/owners/teste", ownerController.getOwners);
 
 //Users Routes
 app.get("/api/users", auth.verifyAdmin, userController.getUsers);
